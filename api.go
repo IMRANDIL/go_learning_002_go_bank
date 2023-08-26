db562ec09b1825de749b61fa0e8bd38f1fd18b03
IMRANDIL <aliimranadil2@gmail.com>
2023-08-26 10:38:23 +0530

    learning golang basic

---
+++ api.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+func (s *APIServer) makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			//handle the error
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			// You can also log the error for debugging purposes
+			// log.Printf("Error: %v", err)
+		}
+	}
+}
+
+type APIServer struct {
+	listenAddr string
+	router     *mux.Router
+}
+
+func newAPIServer(listenAddr string) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+		router:     mux.NewRouter(),
+	}
+}
+
+func (s *APIServer) setupRoutes() {
+	s.router.HandleFunc("/", s.makeHTTPHandleFunc(s.handleAccount)).Methods("GET")
+	s.router.HandleFunc("/accounts", s.makeHTTPHandleFunc(s.handleGetAccount)).Methods("GET")
+	s.router.HandleFunc("/accounts", s.makeHTTPHandleFunc(s.handleCreateAccount)).Methods("POST")
+	s.router.HandleFunc("/accounts/{id}", s.makeHTTPHandleFunc(s.handleDeleteAccount)).Methods("DELETE")
+	s.router.HandleFunc("/accounts/transfer", s.makeHTTPHandleFunc(s.handleAccountTransfer)).Methods("POST")
+}
+
+func (s *APIServer) run() {
+
+	s.setupRoutes()
+	fmt.Printf("Server listening on %s...\n", s.listenAddr)
+	http.ListenAndServe(s.listenAddr, s.router)
+
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+
+	_, err := fmt.Fprintf(w, "Hi ali")
+	return err
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+
+	return nil
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+
+	return nil
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+
+	return nil
+}
+
+func (s *APIServer) handleAccountTransfer(w http.ResponseWriter, r *http.Request) error {
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module imrandil/github.com/go_bank
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
